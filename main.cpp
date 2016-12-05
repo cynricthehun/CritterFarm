55cbe0afd6c42b6ed34877388707595d5651f6ae
Joey Meyer <joeymeyer1986@gmail.com>
2016-12-04 22:04:13 -0600

    basic generation mechanics finished

--- main.cpp
+++ main.cpp
@@ -4,6 +4,61 @@
 
 using namespace std;
 
-class main (){
+class Critter{
+  public:
+    Critter(const string& name="");
+    string GetName() const;
 
+  private:
+    string m_Name;
+};
+
+Critter::Critter(const string& name):m_Name(name){
+
+}
+
+inline string Critter::GetName() const {
+  return m_Name;
+}
+
+class Farm{
+  public:
+    Farm(int spaces = 1);
+    void Add(const Critter& aCritter);
+    void RollCall() const;
+  private:
+    vector<Critter> m_Critters;
+};
+
+Farm::Farm(int spaces){
+  m_Critters.reserve(spaces);
+}
+
+void Farm::Add(const Critter& aCritter){
+  m_Critters.push_back(aCritter);
+}
+
+void Farm::RollCall() const {
+  for (vector<Critter>::const_iterator iter = m_Critters.begin();
+    iter != m_Critters.end();
+    ++iter)
+    cout << iter->GetName() << " here.\n";
+}
+
+int main(){
+  Critter Crit("Poochie");
+  cout << "My Critter's name is " << Crit.GetName() << endl;
+
+  cout << "\nCreating Critter Farm.\n";
+  Farm myFarm(3);
+
+  cout << "\nAdding Critters to Farm.\n";
+  myFarm.Add(Critter("Lilly"));
+  myFarm.Add(Critter("James"));
+  myFarm.Add(Critter("Harry"));
+
+  cout << "\nCalling Roll..\n";
+  myFarm.RollCall();
+
+  return 0;
 }
